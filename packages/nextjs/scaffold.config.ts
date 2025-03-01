e98bb3ac9e314a193b9dc8a45f8440218819d8bd
port <108868128+portdeveloper@users.noreply.github.com>
2025-03-01 15:58:05 +0300

    Make things easier to understand

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -32,7 +32,7 @@ export const monadTestnet = defineChain({
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.foundry, monadTestnet],
+  targetNetworks: [monadTestnet],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
